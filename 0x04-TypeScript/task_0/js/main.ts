e9de19377bee72b393ce285126ac07a2430cef5e
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-25 22:11:52 +0200

    Update main.ts

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,1 +1,41 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
 
+const student1: Student = {
+    firstName: "Sharon",
+    lastName: "Cherono",
+    age: 21,
+    location: "Kenya",
+};
+
+const student2: Student = {
+    firstName: "Mercy",
+    lastName: "Kendi",
+    age: 19,
+    location: "South Africa",
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const body = document.querySelector('body');
+const table = document.createElement('table');
+
+studentsList.forEach(student => {
+            const row = document.createElement('tr');
+            
+            const FirstNameCell = document.createElement('td');
+            FirstNameCell.textContent = student.firstName;
+            
+            const locationCell = document.createElement('td');
+            locationCell.textContent = student.location;
+            
+            row.appendChild(FirstNameCell);
+            row.appendChild(locationCell);
+            table.appendChild(row);
+    });
+
+body.appendChild(table);
